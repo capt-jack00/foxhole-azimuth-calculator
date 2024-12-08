61ff579635a551b5a5b7f58ace7bd3887adf2334
Kociarz <dbednorz@tuta.io>
2024-12-08 14:48:57 +0100

    first commit

---
+++ azimuth-calculator/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace azimuth_calculator;
+
+class Program
+{
+    public static int oppositeAzi;
+
+    static void Main(string[] args)
+    {
+        System.Console.Write("Enter an azmiuth: ");
+        int azi = int.Parse(Console.ReadLine());
+
+        if (azi >= 180)
+        {
+            oppositeAzi = azi - 180;
+        }
+        else if (azi < 180)
+        {
+            oppositeAzi = azi + 180;
+        }
+
+        System.Console.WriteLine($"Opposite azimuth for {azi} is: {oppositeAzi}");
+    }
+}
